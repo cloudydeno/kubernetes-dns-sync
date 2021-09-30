364832f8c4617e75902a7e0eb8293535d32982cc
Daniel Lamando <dan@danopia.net>
2021-09-30 17:46:19 +0200

    Improve endpoints tests, and add to CI

--- common/endpoints_test.ts
+++ common/endpoints_test.ts
@@ -1,24 +1,77 @@
 import { assertEquals } from "https://deno.land/std@0.105.0/testing/asserts.ts";
+
+import type { Endpoint } from "./contract.ts";
 import { SplitByIPVersion, SplitOutTarget } from "./endpoints.ts";
 
-Deno.test('Dualstack targets', () => {
+Deno.test('Endpoint SplitByIPVersion: Dualstack targets', () => {
+  verifySplitByIPVersion({
+    inputTargets: ['127.0.0.1', '::1', '::2'],
+    expectedIPv4Targets: ['127.0.0.1'],
+    expectedIPv6Targets: ['::1', '::2'],
+  });
+
+  verifySplitByIPVersion({
+    inputTargets: ['::1', '127.0.0.1'],
+    expectedIPv4Targets: ['127.0.0.1'],
+    expectedIPv6Targets: ['::1'],
+  });
+});
+
+Deno.test('Endpoint SplitByIPVersion: IPv4-only targets', () => {
+  verifySplitByIPVersion({
+    inputTargets: ['1.1.1.1', '2.2.2.2'],
+    expectedIPv4Targets: ['1.1.1.1', '2.2.2.2'],
+    expectedIPv6Targets: [],
+  });
+});
+
+Deno.test('Endpoint SplitByIPVersion: IPv6-only targets', () => {
+  verifySplitByIPVersion({
+    inputTargets: ['::1', '::2'],
+    expectedIPv4Targets: [],
+    expectedIPv6Targets: ['::1', '::2'],
+  });
+});
+
+Deno.test('Endpoint SplitByIPVersion: Zero targets', () => {
+  verifySplitByIPVersion({
+    inputTargets: [],
+    expectedIPv4Targets: [],
+    expectedIPv6Targets: [],
+  });
+});
+
+
+/**
+ * Helper to assert SplitByIPVersion's splitting behavior
+ */
+function verifySplitByIPVersion(opts: {
+  inputTargets: string[],
+  expectedIPv4Targets: string[],
+  expectedIPv6Targets: string[],
+}) {
   const splitEndpoints = SplitByIPVersion({
     DNSName: 'example.com',
     RecordType: 'A',
-    Targets: ['127.0.0.1', '::1'],
-    RecordTTL: 60,
-    Labels: {
-      'external-dns/resource': `test-suite`,
-    },
+    Targets: opts.inputTargets,
     SplitOutTarget,
   });
 
-  assertEquals(splitEndpoints.length, 2);
+  // Check number of resulting endpoints
+  const expectedCount = [opts.expectedIPv4Targets, opts.expectedIPv6Targets]
+    .map<number>(x => x.length > 0 ? 1 : 0)
+    .reduce((a,b) => a+b, 0);
+  assertEquals(splitEndpoints.length, expectedCount, 'Wrong number of endpoints emitted');
 
-  const v4Endpoints = splitEndpoints.filter(x => x.RecordType === 'A');
-  assertEquals(v4Endpoints.length, 1);
+  if (opts.expectedIPv4Targets.length > 0) {
+    const v4Endpoints = splitEndpoints.filter(x => x.RecordType === 'A');
+    assertEquals(v4Endpoints.length, 1);
+    assertEquals(v4Endpoints[0].Targets, opts.expectedIPv4Targets);
+  }
 
-  const v6Endpoints = splitEndpoints.filter(x => x.RecordType === 'AAAA');
-  assertEquals(v6Endpoints.length, 1);
-
-});
+  if (opts.expectedIPv6Targets.length > 0) {
+    const v6Endpoints = splitEndpoints.filter(x => x.RecordType === 'AAAA');
+    assertEquals(v6Endpoints.length, 1);
+    assertEquals(v6Endpoints[0].Targets, opts.expectedIPv6Targets);
+  }
+}
