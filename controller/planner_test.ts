f0aef6ad1a72c4ef2de5e5a09c18a0c87f777184
Daniel Lamando <dan@danopia.net>
2021-12-07 00:11:11 +0100

    Add a few tests around the codebase
    * Write one mocked test around the Vultr provider
    * Add some basic Planner tests
    * Add an integration test for the main logic loop

---
+++ controller/planner_test.ts
@@ -0,0 +1,114 @@
+import { assertEquals, assertThrows } from "https://deno.land/std@0.105.0/testing/asserts.ts";
+
+import { Planner } from "./planner.ts";
+
+Deno.test("first-time plan (happy path)", () => {
+  const planner = new Planner([{
+    DNSName: "example.com",
+    ZoneID: "ex",
+  }]);
+
+  const changes = planner.PlanChanges([{
+    DNSName: "app.example.com",
+    RecordType: "A",
+    Targets: ["1.1.1.1"],
+  }], []);
+
+  assertEquals(changes.Create.length, 1);
+  assertEquals(changes.Update.length, 0);
+  assertEquals(changes.Delete.length, 0);
+
+  const [creation] = changes.Create;
+  assertEquals(creation.DNSName, "app.example.com");
+  assertEquals(creation.RecordType, "A");
+  assertEquals(creation.Targets, ["1.1.1.1"]);
+});
+
+Deno.test("first-time plan (unknown domain)", () => {
+  const planner = new Planner([{
+    DNSName: "example.com",
+    ZoneID: "ex",
+  }]);
+
+  const changes = planner.PlanChanges([{
+    DNSName: "app.another.com",
+    RecordType: "A",
+    Targets: ["1.1.1.1"],
+  }], []);
+
+  assertEquals(changes.Create.length, 0);
+  assertEquals(changes.Update.length, 0);
+  assertEquals(changes.Delete.length, 0);
+});
+
+Deno.test("no-change plan", () => {
+  const planner = new Planner([{
+    DNSName: "example.com",
+    ZoneID: "ex",
+  }]);
+
+  const changes = planner.PlanChanges([{
+    DNSName: "app.example.com",
+    RecordType: "A",
+    Targets: ["1.1.1.1"],
+  }], [{
+    DNSName: "app.example.com",
+    RecordType: "A",
+    Targets: ["1.1.1.1"],
+    Labels: {'is-ours': 'yes'},
+  }]);
+
+  assertEquals(changes.Create.length, 0);
+  assertEquals(changes.Update.length, 0);
+  assertEquals(changes.Delete.length, 0);
+});
+
+Deno.test("plan with record updates", () => {
+  const planner = new Planner([{
+    DNSName: "example.com",
+    ZoneID: "ex",
+  }]);
+
+  const changes = planner.PlanChanges([{
+    DNSName: "app.example.com",
+    RecordType: "A",
+    Targets: ["2.2.2.2", "3.3.3.3"],
+  }], [{
+    DNSName: "app.example.com",
+    RecordType: "A",
+    Targets: ["1.1.1.1"],
+    Labels: {'is-ours': 'yes'},
+  }]);
+
+  assertEquals(changes.Create.length, 0);
+  assertEquals(changes.Update.length, 1);
+  assertEquals(changes.Delete.length, 0);
+
+  const [update] = changes.Update;
+  assertEquals(update[0].DNSName, "app.example.com");
+  assertEquals(update[1].DNSName, "app.example.com");
+  assertEquals(update[0].RecordType, "A");
+  assertEquals(update[1].RecordType, "A");
+  assertEquals(update[0].Targets, ["1.1.1.1"]);
+  assertEquals(update[1].Targets, ["2.2.2.2", "3.3.3.3"]);
+});
+
+Deno.test("conflicting records", () => {
+  const planner = new Planner([{
+    DNSName: "example.com",
+    ZoneID: "ex",
+  }]);
+
+  assertThrows(() => {
+    const changes = planner.PlanChanges([{
+      DNSName: "app.example.com",
+      RecordType: "A",
+      Targets: ["1.1.1.1"],
+    }], [{
+      DNSName: "app.example.com",
+      RecordType: "A",
+      Targets: ["1.1.1.1"],
+      Labels: {'is-ours': ''},
+    }]);
+  }, Error, 'clash with unmanaged records');
+});
