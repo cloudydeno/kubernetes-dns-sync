6a2929da25758f47226cbb3c33c841265531135b
Daniel Lamando <dan@danopia.net>
2022-01-23 15:34:38 +0100

    Add cloudflare test for MX records

--- src/providers/cloudflare/mod_test.ts
+++ src/providers/cloudflare/mod_test.ts
@@ -77,3 +77,56 @@ Deno.test('cloudflare partial record update', async () => {
 
   apiMock.verifyCompletion();
 });
+
+Deno.test('cloudflare MX records', async () => {
+
+  const apiMock = new CloudflareApiMock();
+  const provider = new CloudflareProvider({
+    type: 'cloudflare',
+  }, apiMock);
+
+  apiMock.addMockedZone('ex', 'example.com', [{
+    expect: 'retained',
+    data: { name: 'example.com', type: 'MX', content: 'mx1a', priority: 5, ttl: 1 },
+  }, {
+    expect: 'retained',
+    data: { name: 'example.com', type: 'MX', content: 'mx1b', priority: 5, ttl: 1 },
+  }, {
+    expect: 'deletion',
+    data: { name: 'example.com', type: 'MX', content: 'mx2-legacy', priority: 10, ttl: 1 },
+  }, {
+    expect: 'creation',
+    data: { name: 'example.com', type: 'MX', content: 'mx2-new', priority: 10, ttl: 1 },
+  }]);
+
+  await applyToProvider(provider, new NoopRegistry({type: 'noop'}), [{
+    annotations: {},
+    resourceKey: 'test',
+    dns: {
+      fqdn: 'example.com',
+      type: 'MX',
+      target: 'mx1a',
+      priority: 5
+    },
+  }, {
+    annotations: {},
+    resourceKey: 'test',
+    dns: {
+      fqdn: 'example.com',
+      type: 'MX',
+      target: 'mx1b',
+      priority: 5
+    },
+  }, {
+    annotations: {},
+    resourceKey: 'test',
+    dns: {
+      fqdn: 'example.com',
+      type: 'MX',
+      target: 'mx2-new',
+      priority: 10
+    },
+  }]);
+
+  apiMock.verifyCompletion();
+});
